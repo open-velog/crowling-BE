62743a4e46a46d8f9a7866f839f427739194fcda
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-15 23:59:16 +0900

    refactor: ConstantTable class 삭제하고 application.properties로 상수 값 관리

--- src/main/java/com/example/finding/constant/ConstantTable.java
+++
@@ -1,7 +0,0 @@
-package com.example.finding.constant;
-
-public class ConstantTable {
-    public static long START_KEYWORD_ID = 3011;
-    public static long BATCH_SIZE = 100000;
-    public static long INTERVAL = 75;
-}
